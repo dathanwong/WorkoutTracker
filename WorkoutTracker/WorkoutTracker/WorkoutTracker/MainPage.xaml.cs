7e43c7adfb5e87b408b73665c6be698f9afd1a89
dathanwong <dwong5@villanova.edu>
2018-07-24 22:03:01 -0700

    testing based on https://www.c-sharpcorner.com/blogs/handling-child-control-event-in-listview-using-xamarinforms1 currently listview is not updating

--- WorkoutTracker/WorkoutTracker/WorkoutTracker/MainPage.xaml.cs
+++ WorkoutTracker/WorkoutTracker/WorkoutTracker/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,21 +11,62 @@ namespace WorkoutTracker
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+
+        List<Exercise> workout = new List<Exercise>();
+        ExerciseList exercises;
+
+        public MainPage()
 		{
 			InitializeComponent();
-            List<Exercise> workout = new List<Exercise>();
-            workout.Add(new Exercise { Name = "bicep curl" });
-            workout.Add(new Exercise { Name = "lat pull" });
-            workout.Add(new Exercise { Name = "bench press" });
-            workout.Add(new Exercise { Name = "shoulder press" });
-            Exercises.ItemsSource = workout;
+            workout.Add(new Exercise { Name = "bicep curl", Reps = 5, Weight = 100, Index = 0});
+            workout.Add(new Exercise { Name = "lat pull", Reps = 50, Weight = 100, Index = 1 });
+            workout.Add(new Exercise { Name = "bench press", Reps = 213, Weight = 100, Index = 2 });
+            workout.Add(new Exercise { Name = "shoulder press", Reps = 4, Weight = 100, Index = 3 });
+            exercises = new ExerciseList(workout);
+            Exercises.ItemsSource = exercises.Exercises;
 		}
 
+        private void BtnMinusWeight_Clicked(object sender, EventArgs e)
+        {
+            int selectedItem = (int) (sender as Button).CommandParameter;
+            Exercise exercise = exercises.Exercises[selectedItem];
+            exercises.Exercises[selectedItem].Weight = exercise.Weight - 5;
+        }
     }
-    
+
     public class Exercise
     {
+        public int Index { get; set; }
         public string Name { get; set; }
+        public int Reps { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class ExerciseList : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<Exercise> _exercises;
+
+        public ObservableCollection<Exercise> Exercises
+        {
+            get { return _exercises; }
+            set { _exercises = value; OnPropertyChanged("Exercises"); }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged == null)
+                return;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ExerciseList(List<Exercise> exerciseList)
+        {
+            Exercises = new ObservableCollection<Exercise>();
+            foreach (Exercise ex in exerciseList)
+            {
+                Exercises.Add(ex);
+            }
+        }
     }
 }
